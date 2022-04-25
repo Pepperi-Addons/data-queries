94c06c0cae4bb523e961aa19d33b9517a3d75e4f
leonPep42 <leon.b@pepperi.com>
2022-04-25 15:12:49 +0300

    packages

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddonService } from '../services/addon.service';
+import { DataVisualizationService } from '../services/data-visualization.service';
 import { PepAddonService } from '@pepperi-addons/ngx-lib';
 import { TranslateModule, TranslateLoader, TranslateStore } from '@ngx-translate/core';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { QueryManagerModule } from './query_manager/query-manager.module'
 import { SeriesEditorModule } from './series-editor/series-editor.module'
+import { PepGenericListService } from '@pepperi-addons/ngx-composite-lib/generic-list';
 
 
 @NgModule({
@@ -32,7 +34,9 @@ import { SeriesEditorModule } from './series-editor/series-editor.module'
     ],
     providers: [
         TranslateStore,
-        AddonService
+        AddonService,
+        DataVisualizationService,
+        PepGenericListService
         // When loading this module from route we need to add this here (because only this module is loading).
     ],
     bootstrap: [AppComponent]
