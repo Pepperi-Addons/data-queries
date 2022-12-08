1410cfcdad1f453f310b398c0801a39577931dbe
Leon Benyaminov <leon.b@pepperi.com>
2022-12-08 11:57:02 +0200

    added .cs to aggregation string fields

--- client-side/src/app/series-editor/series-editor.component.ts
+++ client-side/src/app/series-editor/series-editor.component.ts
@@ -226,7 +226,7 @@ export class SeriesEditorComponent implements OnInit {
     if(this.series.Resource && this.resourcesFields[this.series.Resource]){
           this.aggregationsFieldsOptions["Number"] =  this.resourcesFields[this.series.Resource].filter(f=>f.Type == "Integer" || f.Type == "Double").map(function(f) {return { key: f.FieldID, value: f.FieldID }})
           this.aggregationsFieldsOptions["Date"] =  this.resourcesFields[this.series.Resource].filter(f=>f.Type == "DateTime").map(function(f) {return { key: f.FieldID, value: f.FieldID }})
-          this.aggregationsFieldsOptions["All"] = this.resourcesFields[this.series.Resource].map(function(f) {return { key: f.FieldID, value: f.FieldID }});
+          this.aggregationsFieldsOptions["All"] = this.resourcesFields[this.series.Resource].map(function(f) {return { key: (f.Type=="String" || f.Type=="MultipleStringValues")  ? f.FieldID+'.cs' : f.FieldID, value: f.FieldID }});
     }
   }
 
