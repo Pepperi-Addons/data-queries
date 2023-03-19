44fd43fc3978c9f97e348dc37d1763de5aa23969
Shir Marciano <shir.o@pepperi.com>
2023-03-19 11:13:48 +0200

    Block execute and API

--- server-side/api.ts
+++ server-side/api.ts
@@ -2,33 +2,40 @@ import { Client, Request } from '@pepperi-addons/debug-server'
 import QueryService from './services/query.service'
 
 export async function queries(client: Client, request: Request) {
-    const service = new QueryService(client)
-    if (request.method == 'POST') {
-        return await service.upsert(client, request);
-    }
-    else if (request.method == 'GET') {
-        return await service.find(request.query);
-    }
+
+    throw new Error(`Permission denied. Please contact support for more information.`);       
+    
+    // const service = new QueryService(client)
+    // if (request.method == 'POST') {
+    //     return await service.upsert(client, request);
+    // }
+    // else if (request.method == 'GET') {
+    //     return await service.find(request.query);
+    // }
 }
 
 export function import_data_source(client: Client, request: Request) {
-    const service = new QueryService(client)
-    if (request.method == 'POST') {
-        return service.importDataSource(request.body);
-    }
-    else if (request.method == 'GET') {
-        throw new Error(`Method ${request.method} not supported`);       
-    }
+    throw new Error(`Permission denied. Please contact support for more information.`);       
+
+    // const service = new QueryService(client)
+    // if (request.method == 'POST') {
+    //     return service.importDataSource(request.body);
+    // }
+    // else if (request.method == 'GET') {
+    //     throw new Error(`Method ${request.method} not supported`);       
+    // }
 }
 
 export function export_data_source(client: Client, request: Request) {
-    const service = new QueryService(client)
-    if (request.method == 'POST') {
-        return service.exportDataSource(request.body);
-    }
-    else if (request.method == 'GET') {
-        throw new Error(`Method ${request.method} not supported`);       
-    }
+    throw new Error(`Permission denied. Please contact support for more information.`);       
+
+    // const service = new QueryService(client)
+    // if (request.method == 'POST') {
+    //     return service.exportDataSource(request.body);
+    // }
+    // else if (request.method == 'GET') {
+    //     throw new Error(`Method ${request.method} not supported`);       
+    // }
 }
 
 
--- server-side/elastic.ts
+++ server-side/elastic.ts
@@ -3,26 +3,30 @@ import ElasticService from "./services/elastic.service";
 
 // [Endpoint='elastic/execute?query_id={queryID}']
 export async function execute(client: Client, request: Request) {
-    const service = new ElasticService(client);
-    if (request.method == 'POST') {
-        // VariableValues is the only field allowed to be sent by all users.
-        request.body = {"VariableValues": request.body?.VariableValues};
-        return await service.executeUserDefinedQuery(client, request);
-    }
-    else{
-        throw new Error('Bad request');
-    }
+    throw new Error(`Permission denied. Please contact support for more information. `);       
+
+    // const service = new ElasticService(client);
+    // if (request.method == 'POST') {
+    //     // VariableValues is the only field allowed to be sent by all users.
+    //     request.body = {"VariableValues": request.body?.VariableValues};
+    //     return await service.executeUserDefinedQuery(client, request);
+    // }
+    // else{
+    //     throw new Error('Bad request');
+    // }
 };
 
 export async function execute_debug(client: Client, request: Request) {
-    const service = new ElasticService(client);
-    if (request.method == 'POST') {
-        await client.ValidatePermission('CALL_EXECUTE');
-        return await service.executeUserDefinedQuery(client, request);
-    }
-    else{
-        throw new Error('Bad request');
-    }
+    throw new Error(`Permission denied. Please contact support for more information. `);       
+
+    // const service = new ElasticService(client);
+    // if (request.method == 'POST') {
+    //     await client.ValidatePermission('CALL_EXECUTE');
+    //     return await service.executeUserDefinedQuery(client, request);
+    // }
+    // else{
+    //     throw new Error('Bad request');
+    // }
 
 };
 
