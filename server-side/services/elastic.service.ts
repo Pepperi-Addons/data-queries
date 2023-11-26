07e34dc2eafa8be08006340005fa0997b5adc6a8
Leon Benyaminov <leon.b@pepperi.com>
2023-11-26 15:23:37 +0200

    added missing types

--- server-side/services/elastic.service.ts
+++ server-side/services/elastic.service.ts
@@ -2,9 +2,8 @@ import { PapiClient } from '@pepperi-addons/papi-sdk'
 import { Client, Request } from '@pepperi-addons/debug-server';
 import config from '../../addon.config.json'
 import { AggregatedField, DataQuery, DATA_QUREIES_TABLE_NAME, GroupBy, Interval, Serie } from '../models/data-query';
-import { validate } from 'jsonschema';
+import { ValidatorResult, validate } from 'jsonschema';
 import esb, { Aggregation, Query, RequestBodySearch } from 'elastic-builder';
-import { callElasticSearchLambda } from '@pepperi-addons/system-addon-utils';
 import jwtDecode from 'jwt-decode';
 import { DataQueryResponse, SeriesData } from '../models/data-query-response';
 import { QueryExecutionScheme } from '../models/query-execution-scheme';
@@ -33,12 +32,13 @@ class ElasticService {
     Year: 'yyyy',
     None: ''
   }
+
   hitsFilter: Query = esb.matchAllQuery();
   hitsRequested: boolean = false;
 
-  async executeUserDefinedQuery(client: Client, request: Request) {
+  async executeUserDefinedQuery(client: Client, request: Request): Promise<DataQueryResponse> {
 	const startTime = Date.now();
-    const validation = validate(request.body, QueryExecutionScheme);
+    const validation: ValidatorResult = validate(request.body, QueryExecutionScheme);
 
     if (!validation.valid) {
       throw new Error(validation.toString());
@@ -173,7 +173,7 @@ class ElasticService {
     return queryAggregation;
   }
 
-  private replaceAllVariables(jsonFilter, variableValues: {[varName: string]: string}) {
+  private replaceAllVariables(jsonFilter, variableValues: {[varName: string]: string}): void {
     if (jsonFilter.Operation === 'AND' || jsonFilter.Operation === 'OR') {
       const f1 = this.replaceAllVariables(jsonFilter.LeftNode, variableValues);
       const f2 = this.replaceAllVariables(jsonFilter.RightNode, variableValues);
@@ -205,7 +205,7 @@ class ElasticService {
   }
 
   // if there is scope add user/accounts filters to resourceFilter
-  private async addScopeFilters(series, resourceFilter, resourceRelationData, requestedUserID) {
+  private async addScopeFilters(series, resourceFilter, resourceRelationData, requestedUserID): Promise<Query> {
 	let userID;
 	if(requestedUserID) {
 		userID = requestedUserID;
@@ -287,7 +287,7 @@ class ElasticService {
     return resourceFilter;
   }
 
-  private buildUsersIdsString(usersUnderMyRole, userFieldID) {
+  private buildUsersIdsString(usersUnderMyRole, userFieldID): string {
     let IdsString = '(';
     for(const user of usersUnderMyRole) {
       IdsString += `'${user[userFieldID]}',`;
@@ -295,7 +295,7 @@ class ElasticService {
     return IdsString.slice(0,-1)+')';
   }
 
-  private buildSeriesAggregationList(series, timeZoneOffsetString: string | undefined) {
+  private buildSeriesAggregationList(series, timeZoneOffsetString: string | undefined): { [key: string]: Aggregation[] } {
 
     let aggregationsList: { [key: string]: Aggregation[] } = {};
 
@@ -364,12 +364,12 @@ class ElasticService {
     return aggregationsList;
   }
 
-  private buildBucketSortAggregation(aggName, serie) {
+  private buildBucketSortAggregation(aggName, serie): esb.BucketSortAggregation {
     const order = serie.Top.Ascending === true ? 'asc' : 'desc';
     return esb.bucketSortAggregation('sort').sort([esb.sort(aggName, order)]).size(serie.Top.Max)
   }
 
-  private buildResponseFromElasticResults(lambdaResponse, query: DataQuery, seriesName: string) {
+  private buildResponseFromElasticResults(lambdaResponse, query: DataQuery, seriesName: string): DataQueryResponse {
 
     let response: DataQueryResponse = new DataQueryResponse();
     const seriesToIterate = (seriesName) ? query.Series.filter(s => s.Name==seriesName) : query.Series;
@@ -520,7 +520,7 @@ class ElasticService {
 
   // build sggregation - if the type field is date time build dateHistogramAggregation else termsAggregation
   // sourceAggs determine if its group by or break by so we can distinguish between them in the results
-  private buildAggregationQuery(groupBy: GroupBy, timeZoneOffsetString: string | undefined) {
+  private buildAggregationQuery(groupBy: GroupBy, timeZoneOffsetString: string | undefined): Aggregation {
 
     // Maximum size of each aggregation is 100
     //const topAggs = groupBy.Top?.Max ? groupBy.Top.Max : this.MaxAggregationSize;
@@ -562,11 +562,11 @@ class ElasticService {
     }
   }
 
-  buildDataSetKeyString(keyName: string, pattern: string) {
+  buildDataSetKeyString(keyName: string, pattern: string): string {
     return pattern.replace('${label}', keyName);
   }
 
-  private getAggregator(aggregatedField: AggregatedField, aggName: string) {
+  private getAggregator(aggregatedField: AggregatedField, aggName: string): esb.MetricsAggregationBase {
     let agg;
     switch (aggregatedField.Aggregator) {
       case 'Sum':
@@ -586,7 +586,7 @@ class ElasticService {
     return agg;
   }
 
-  private async getUserDefinedQuery(request: Request) {
+  private async getUserDefinedQuery(request: Request): Promise<DataQuery> {
 
     const queryKey = request.query.key;
 
@@ -614,14 +614,14 @@ class ElasticService {
 	return timeZoneOffsetString;
   }
 
-  private toHoursAndMinutes(totalMinutes) {
+  private toHoursAndMinutes(totalMinutes): string {
 	const minutes = totalMinutes % 60;
 	const hours = Math.floor(totalMinutes / 60);
   
 	return `${this.padTo2Digits(hours)}:${this.padTo2Digits(minutes)}`;
   }
   
-  private padTo2Digits(num) {
+  private padTo2Digits(num): string {
 	return num.toString().padStart(2, '0');
   }
 }
--- server-side/services/pns.service.ts
+++ server-side/services/pns.service.ts
@@ -1,4 +1,4 @@
-import { PapiClient } from '@pepperi-addons/papi-sdk';
+import { PapiClient, Relation } from '@pepperi-addons/papi-sdk';
 import { Client } from '@pepperi-addons/debug-server';
 import config from '../../addon.config.json';
 
@@ -6,7 +6,7 @@ export class PnsService {
     
     papiClient: PapiClient
 
-    constructor(private client: Client) {
+    constructor(client: Client) {
         this.papiClient = new PapiClient({
             baseURL: client.BaseURL,
             token: client.OAuthAccessToken,
@@ -16,7 +16,7 @@ export class PnsService {
         });
     }
 
-    async subscribeToRelationsUpdate() {
+    async subscribeToRelationsUpdate(): Promise<void> {
 		const relationsAddonUUID = "5ac7d8c3-0249-4805-8ce9-af4aecd77794";
         await this.papiClient.notification.subscriptions.upsert({
             AddonUUID: config.AddonUUID,
@@ -31,11 +31,11 @@ export class PnsService {
         })
 	}
 
-	async updateRelationsOnQueries(messageFromPNS: any) {
-		const relationsKeys = messageFromPNS.Message.ModifiedObjects.map(obj => obj.ObjectKey);
-		const relevantRelationsKeys = relationsKeys.filter(key => key.includes('DataQueries'));
-		const relevantRelationsNames = relevantRelationsKeys.map(key => this.extractResourceNameFromKey(key));
-		const queriesToUpdate = await this.papiClient.get(`/data_queries?where=Resource in (${relevantRelationsNames.join(',')})&fields=Key,ResourceData`);
+	async updateRelationsOnQueries(messageFromPNS: any): Promise<void> {
+		const relationsKeys: string[] = messageFromPNS.Message.ModifiedObjects.map(obj => obj.ObjectKey);
+		const relevantRelationsKeys: string[] = relationsKeys.filter(key => key.includes('DataQueries'));
+		const relevantRelationsNames: string[] = relevantRelationsKeys.map(key => this.extractResourceNameFromKey(key));
+		const queriesToUpdate: any[] = await this.papiClient.get(`/data_queries?where=Resource in (${relevantRelationsNames.join(',')})&fields=Key,ResourceData`);
 		const promises = await Promise.all(queriesToUpdate.map(query => this.fixQueryRelation(query)));
 
 		console.log(`Done updating ${promises.length} queries. fixQueryRelation returned: ${JSON.stringify(promises)}`);
@@ -43,15 +43,15 @@ export class PnsService {
 
 	extractResourceNameFromKey(key: string): string {
 		// In our case the key format is: <resource_name>_<addon_uuid>_DataQueries
-		let keyParts = key.split('_');
+		let keyParts: string[] = key.split('_');
 		keyParts.pop(); // remove the relation name (DataQueries)
 		keyParts.pop(); // remove the addon uuid
 		return keyParts.join('_');
 	}
 
 	async fixQueryRelation(query: any): Promise<boolean> {
-		const relationSavedOnQuery = query.ResourceData;
-		const updatedRelation = await this.papiClient.get(`/addons/data/relations?key=${relationSavedOnQuery.Key}`);
+		const relationSavedOnQuery: Relation = query.ResourceData;
+		const updatedRelation: Relation = await this.papiClient.get(`/addons/data/relations?key=${relationSavedOnQuery.Key}`);
 
 		console.log(`Updating resource relation data on query: ${query.Key}`);
 		query.ResourceData = updatedRelation;
