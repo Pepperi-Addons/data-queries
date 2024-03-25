9a0a9b185888dfc668a52de4d6d8a631258f6729
Leon Benyaminov <leon.b@pepperi.com>
2024-03-25 16:21:18 +0200

    var settings fix

--- server-side/services/varSettings.service.ts
+++ server-side/services/varSettings.service.ts
@@ -1,4 +1,4 @@
-import { PapiClient } from '@pepperi-addons/papi-sdk';
+import { AddonData, DIMXObject, PapiClient } from '@pepperi-addons/papi-sdk';
 import { Client } from '@pepperi-addons/debug-server';
 import config from '../../addon.config.json'
 import { DATA_QUREIES_TABLE_NAME, DataQuery } from '../models';
@@ -18,7 +18,7 @@ export class VarSettingsService {
         });
     }
 
-	async upsertVarSettings(varSettings: VarSettingsObject): Promise<any> {
+	async upsertVarSettings(varSettings: VarSettingsObject): Promise<DIMXObject[]> {
 		await this.setKmsParameter('License', varSettings.License);
 		await this.setKmsParameter('DaysLimit', varSettings.DaysLimit);
 		await this.setKmsParameter('TrialEndDate', varSettings.TrialEndDate);
@@ -34,20 +34,14 @@ export class VarSettingsService {
 	}
 
 	async getVarSettings(): Promise<VarSettingsObject> {
-		// const license = await this.getKmsParameter('License');
-		// const daysLimit = await this.getKmsParameter('DaysLimit');
-		// const trialEndDate = await this.getKmsParameter('TrialEndDate');
-
-		// return {
-		// 	License: license.Value,
-		// 	DaysLimit: daysLimit.Value,
-		// 	TrialEndDate: trialEndDate.Value
-		// };
+		const license = await this.getKmsParameter('License');
+		const daysLimit = await this.getKmsParameter('DaysLimit');
+		const trialEndDate = await this.getKmsParameter('TrialEndDate');
 
 		return {
-			License: 'Free version',
-			DaysLimit: '90',
-			TrialEndDate: new Date().toLocaleString()
+			License: license.Value,
+			DaysLimit: daysLimit.Value,
+			TrialEndDate: trialEndDate.Value
 		};
 	}
 
@@ -66,7 +60,8 @@ export class VarSettingsService {
 		});
 	}
 
-	async getKmsParameter(key: string): Promise<any> {
+	async getKmsParameter(key: string): Promise<AddonData> {
+		// TODO: use kms api when it's ready
 		return await this.papiClient.get(`/addons/api/8b4a1bd8-a2eb-4241-85ac-89c9e724e900/api/addon_data?addon_uuid=${config.AddonUUID}&key=${key}`);
 	}
 }
